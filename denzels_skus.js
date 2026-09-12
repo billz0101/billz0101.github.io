f47cc4b3c138835041a8834800fe69f3e5714d8e
billz0101 <66141327+billz0101@users.noreply.github.com>
2026-09-12 23:00:10 +0100

    Add files via upload

---
+++ denzels_skus.js
@@ -0,0 +1,39 @@
+// Denzels SKU -> pack multiplier map, mirrored from denzels_skus.json
+// (bundled as a script because index.html is opened via file:// where fetch
+//  of the JSON is blocked). multiplier = the pack size sold per case, e.g.
+//  BATBITES8 means 8 items per unit, DNTSMLPUP28 means 28, singles are 1.
+window.DENZELS_SKUS = {
+  "XMASADV1": 1,
+  "TRAYBCC10": 10,
+  "VALHEARTS1": 1,
+  "VALBOX1": 1,
+  "VALHEARTS6": 6,
+  "LFPDCAP-GN": 1,
+  "COINBITES1": 1,
+  "SPMXBITES1": 1,
+  "LTTRBITES1": 1,
+  "MXTPBITES1": 1,
+  "SBBPB1": 1,
+  "SBBPC1": 1,
+  "SBBBM1": 1,
+  "SBBBB1": 1,
+  "PUPCHEWS1": 1,
+  "PUPPACK1": 1,
+  "BATBITES1": 1,
+  "TRAYBCC1": 1,
+  "HLWCHEWS10": 10,
+  "HLWCHEWS1": 1,
+  "VALRWDSC1": 1,
+  "DNTSMLPUP28": 28,
+  "DNTMEDVG28": 28,
+  "DNTMEDCH28": 28,
+  "BDAYGIFT1": 1,
+  "DNTMEDVG1": 1,
+  "DNTMEDCH1": 1,
+  "DNTSMLPUP1": 1,
+  "RWDTA1": 1,
+  "RWDDP1": 1,
+  "RWDCM1": 1,
+  "RWDSC1": 1,
+  "PUPBITES1": 1
+};
